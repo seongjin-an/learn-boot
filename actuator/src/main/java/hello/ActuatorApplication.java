659fe471b0dd98903a8b5c4902491619ff3fd23e
seongjin-an <seongjin4us@gmail.com>
2023-04-27 20:31:03 +0900

    메트릭 등록1 - 카운터

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,15 @@
 package hello;
 
 import hello.order.v0.OrderServiceV0;
+import hello.order.v1.OrderServiceV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(OrderServiceV0.class)
+//@Import(OrderServiceV0.class)
+@Import(OrderServiceV1.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
